554673f66f9506534089b56c40efe2dcd7bc1c8e
小久保力輝 <kokub0303@gmail.com>
2023-04-20 20:28:33 +0900

    userモデルとprofileモデルの関連付け

--- db/migrate/20230420105154_create_profiles.rb
+++ db/migrate/20230420105154_create_profiles.rb
@@ -1,6 +1,8 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
+      t.references :user, null: false
+      # 必要なパラメーターは画像のみ、activestoregeで設定
 
       t.timestamps
     end
