8e3348a4a59318270eaa79d9222c92680b7ff763
小久保力輝 <kokub0303@gmail.com>
2023-04-26 21:22:59 +0900

    いいね出来るように

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  def has_liked?(post)
+    likes.exists?(post_id: post.id)
+  end
+
 end
