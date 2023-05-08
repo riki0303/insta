d61baa0b9959b1651ba3c88939613af18893993c
小久保力輝 <kokub0303@gmail.com>
2023-05-08 11:44:52 +0900

    フォロー一覧ページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   # 中間テーブルとの関係（自分が相手をフォローする時）
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship' , dependent: :destroy
   # 中間テーブルをまたいでfollowingを取得
-  has_many :followings, through: :following_relationsips, source: :following
+  has_many :followings, through: :following_relationships, source: :following
 
   has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :follower_relationships, source: :follower
