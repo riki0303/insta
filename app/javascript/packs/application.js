09261a6c25243b91777eeeee6cef5b069b879485
小久保力輝 <kokub0303@gmail.com>
2023-05-17 19:44:12 +0900

    ajaxでいいねの状態

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,9 @@ require("channels");
 
 import $ from "jquery";
 import axios from "axios";
+import { csrfToken } from "rails-ujs";
+
+axios.defaults.headers.common["X-CSRF-Token"] = csrfToken();
 
 const handleLikeDisplay = (hasLiked) => {
   if (hasLiked) {
@@ -32,5 +35,31 @@ document.addEventListener("turbolinks:load", () => {
   axios.get(`/posts/${postId}/like`).then((response) => {
     const hasLiked = response.data.hasLiked;
     handleLikeDisplay(hasLiked);
+  });
+
+  $(".like-inactive").on("click", () => {
+    axios
+      .post(`/posts/${postId}/like`)
+      .then((response) => {
+        window.alert("OK!!");
+        console.log(response);
+      })
+      .catch((e) => {
+        window.alert("error!!!");
+        console.log(e);
+      });
+  });
+
+  $(".like-active").on("click", () => {
+    axios
+      .delete(`/posts/${postId}/like`)
+      .then((response) => {
+        window.alert("OK!!");
+        console.log(response);
+      })
+      .catch((e) => {
+        window.alert("error!!!");
+        console.log(e);
+      });
   });
 });
