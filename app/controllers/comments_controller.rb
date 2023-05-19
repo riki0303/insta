c7b74dfdb97e55a99c7d23962090f8cde469a076
小久保力輝 <kokub0303@gmail.com>
2023-05-19 09:18:58 +0900

    コメントを非同期通信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,23 +11,26 @@ class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build
-    @comment.user_id = current_user.id
-    # コメント一覧表示用
-    @comments = @post.comments
-  end
+  #   @comment.user_id = current_user.id
+  #   # コメント一覧表示用
+  #   @comments = @post.comments
+   end
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
-    # render :newの後のコメント一覧表示用
-    @comments = @post.comments
-    if @comment.save
-      redirect_to new_post_comment_path(@post), notice: 'Success!'
-    else
-      flash.now[:error] = 'Failure!'
-      render :new
-    end
+
+    @comment.save!
+    render json: @comment
+    # # render :newの後のコメント一覧表示用
+    # @comments = @post.comments
+    # if @comment.save
+    #   redirect_to new_post_comment_path(@post), notice: 'Success!'
+    # else
+    #   flash.now[:error] = 'Failure!'
+    #   render :new
+    # end
   end
 
   private
