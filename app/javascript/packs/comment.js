4553f2d3da1074204933ce4fa9f73a1829093f53
小久保力輝 <kokub0303@gmail.com>
2023-05-22 22:27:54 +0900

    コメントのプロフィール画像にjsでリンク付けた

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -3,13 +3,17 @@ import axios from "modules/axios";
 
 const appendNewComment = (comment) => {
   $(".comment").append(
-    `<div class="comment__item">
-      <div class="comment__left">
-        <img src="${comment.user_avatar}" class="comment__avatar"></img>
+    `<div class= "comment__item">
+      <div class= "comment__left">
+        <a class = "comment__avatar-container href= "/accounts/${comment.user_id}">
+          <img src="${comment.user_avatar}" class="comment__avatar"></img>
+        </a>
       </div>
-      <div class="comment__right">
-        <div class = "comment__account">${comment.user_account}</>
-        <p class="comment__content">${comment.content}</p>
+      <div class= "comment__right">
+        <a class = "comment__account href= "/accounts/${comment.user_id}">
+          <div class = "comment__account">${comment.user_account}</>
+        </a>
+        <p class= "comment__content">${comment.content}</p>
       </div>
     </div>`
   );
