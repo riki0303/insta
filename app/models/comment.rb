c8c7ea60e8f8e93219e4b686cb7cd623f972aac4
小久保力輝 <kokub0303@gmail.com>
2023-05-24 15:40:52 +0900

    ールバックを使ってメールを送信できるように

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,4 +23,11 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
+
+  after_create :send_email
+
+  private
+  def send_email
+    CommentMailer.new_comment(user).deliver_now
+  end
 end
