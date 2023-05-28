fa576bb51b5c2c43ab0f9b0f149e0507dd7dedc0
小久保力輝 <kokub0303@gmail.com>
2023-05-29 07:30:21 +0900

    posts#indexのテストを追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, user: user) }
+
   describe 'GET /posts' do
     it '200ステータスが返ってくる' do
       get posts_path
