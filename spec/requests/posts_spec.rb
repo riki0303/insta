e68589dfae2925479d4d291eb316989a703ac452
小久保力輝 <kokub0303@gmail.com>
2023-05-29 07:51:24 +0900

    post リクエストのテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST /posts' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+
+      it '記事が保存される' do
+        post_params = attributes_for(:post)
+        post posts_path({post: post_params})
+        expect(response).to have_http_status(302)
+        # 302が返ってきても記事が保存されているかは分からない為以下を実行
+        expect(Post.last.title).to eq(post_params[:title])
+      end
+    end
+  end
 end
+
