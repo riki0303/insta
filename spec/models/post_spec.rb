835155e73542e9ef5544c557ef612b9a06a3f991
小久保力輝 <kokub0303@gmail.com>
2023-05-28 11:18:20 +0900

    保存に失敗する場合のテストを追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'タイトルが入力されていれば、記事を保存できる' do
+  let!(:user) do
     user = User.create! ({
-      email: 'test@example.com',
-      password: 'password',
-      account: 'account'
+    email: 'test@example.com',
+    password: 'password',
+    account: 'account'
     })
+  end
 
-    post = user.posts.build({
-      title: Faker::Lorem.characters(number: 10)
-    })
+  context 'タイトルが入力されてる場合' do
+    let!(:post) do
+      user.posts.build({
+        title: Faker::Lorem.characters(number: 10)
+      })
+    end
 
-    expect(post).to be_valid
+    it '記事を保存出来る' do
+      expect(post).to be_valid
+    end
+  end
+
+
+  context 'タイトルが入力されていない場合' do
+    let!(:post) do
+      user.posts.create({
+        title: ''
+      })
+    end
+
+    it '記事を保存出来ない' do
+      expect(post.errors.messages[:title][0]).to eq('を入力してください')
+      
+    end
   end
 end
