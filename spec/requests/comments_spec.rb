89a8b0b0a56c30ab208b77707c84a3d2e2e857c3
小久保力輝 <kokub0303@gmail.com>
2023-05-29 20:24:08 +0900

    commentコントローラーのテスト

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Comments', type: :request do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user: user) }
+  let(:comments) { create_list(:comment, 3, post: post, user: user) }
+
+  describe 'GET /comments' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      it '200 Status' do
+        get post_comments_path(post_id: post.id)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it '200 Status' do
+        get post_comments_path(post_id: post.id)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
