bb9dbd4b392c3b9669ef962a879aefd80f176a8b
小久保力輝 <kokub0303@gmail.com>
2023-05-31 12:00:31 +0900

    traitの使い方、profileページのsystemtest

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :profile do
+    name { Faker::Name.name }
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password{ 'password' }
     account { Faker::Lorem.characters(number: 5)}
+
+    trait :with_profile do
+      after :build do |user|
+        build(:profile, user: user)
+      end
+    end
   end
 end
