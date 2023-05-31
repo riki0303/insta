adb3fa28efad09deb18a4becdc3f39e6d9013ed7
小久保力輝 <kokub0303@gmail.com>
2023-05-31 09:41:14 +0900

    記事一覧のタイトルが表示されているか確認

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :system do
+  let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, user: user) }
+
   it '記事一覧が表示される' do
     visit root_path
+    posts.each do |post|
+      expect(page).to have_content(post.title)
+    end
   end
 end
